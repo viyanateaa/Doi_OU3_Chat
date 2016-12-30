b4e168c555ecd1a104876e1c1861103002d61490
viyanateaa <viyanateaa@gmail.com>
2016-12-30 22:35:20 +0100

    GetListTest
    GetList JunitTest is done!

--- test/GetListPDU_Juinttest.java
+++ test/GetListPDU_Juinttest.java
@@ -5,14 +5,54 @@ import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 /**
  * Created by viyanateaa on 2016-12-22.
  */
 public class GetListPDU_Juinttest {
     @Test
-    public void create_new_getlist_test() throws UnsupportedEncodingException {
+    public void ShouldBeAbleToCreateGetList() throws UnsupportedEncodingException {
         GetList getList = new GetList();
+    }
 
+
+    @Test
+    public void ShouldHaveCorrectOp (){
+        GetList getList = new GetList();
+        byte[] numbrOfOp = getList.getBytes();
+        assertEquals((numbrOfOp[0]), 3);
+    }
+
+
+    @Test
+    public void shouldGetWrongOpNumber() throws Exception {
+        GetList getList = new GetList();
+        byte[] list = getList.getBytes();
+        assertNotEquals((list[1]), 3);
+    }
+
+
+    @Test
+    public void shouldBeAbleToCreateGetListCorrect() throws Exception {
+        GetList getList = new GetList();
+        byte[] list = getList.getBytes();
+        assertEquals((list[0]), 3);
+        for (int i = 1; i <4 ; i++) {
+            assertEquals(list[i], 0);
+        }
+
+    }
+
+    @Test
+    public void shouldBeAbleToGetWrongList() throws Exception {
+        GetList getList = new GetList();
+        byte[] list = getList.getBytes();
+        assertEquals((list[0]), 3);
+        for (int i = 1; i <4 ; i++) {
+            assertNotEquals(list[i], 5);
+        }
 
     }
 }
