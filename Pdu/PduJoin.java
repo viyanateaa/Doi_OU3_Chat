61add014c77e538ccfd7a9f6b8e5566720cb4674
viyanateaa <viyanateaa@gmail.com>
2017-01-01 11:29:59 +0100

    No code changed in pdu join, just upload joinpdu test in pdu file

--- Pdu/PduJoin.java
+++ Pdu/PduJoin.java
@@ -5,7 +5,7 @@ import java.io.UnsupportedEncodingException;
  * Created by kristoffer & Viyan on 2016-09-28.
  */
 public class PduJoin extends Pdu{
-
+//did mistake, uploade wrong file
     private final byte op = 12;
 
     /**
