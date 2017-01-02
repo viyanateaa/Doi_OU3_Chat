a64a53b29cacc027b150d9e6e91937cbbbd8da8e
viyanateaa <viyanateaa@gmail.com>
2017-01-02 16:24:04 +0100

    change in participent
    some changes!

--- Pdu/PduParticipants.java
+++ Pdu/PduParticipants.java
@@ -12,16 +12,24 @@ public class PduParticipants extends Pdu{
     private String[] participants;
 
     public PduParticipants(InputStream inputStream) throws IOException {
+        sequenceBuilder = new ByteSequenceBuilder((byte)19);
 
         byte nrOfClients = (byte)inputStream.read();
+        sequenceBuilder.append(nrOfClients);
+
         participants = new String[nrOfClients];
         byte[] lenghtArray = new byte[2];
+
+
 
         //collects info about length of pdu
         lenghtArray[0] = (byte)inputStream.read();
         lenghtArray[1] = (byte)inputStream.read();
+
         int lenghtOfPdu = ((lenghtArray[0] & 0xff) | (lenghtArray[1]
                 & 0xff));
+
+        sequenceBuilder.append(lenghtArray);
 
         //split info into separate strings
         for(int i=0;i<nrOfClients;i++){
@@ -29,25 +37,31 @@ public class PduParticipants extends Pdu{
             int index = 0;
             while (true){
                 name[index] = (byte)inputStream.read();
+                sequenceBuilder.append(name[index]);
                 if(name[index] == 0){
                     break;
                 }
                 index++;
             }
-            participants[i] = new String(name, StandardCharsets
-                    .UTF_8);
+            participants[i] = new String(name, StandardCharsets.UTF_8);
+
         }
+
+
         // takes care of if there is remaining padding. Checks it
         // as well.
         if(lenghtOfPdu%4 != 0){
             for (int j = 0; j < (4 - (lenghtOfPdu % 4)); j++) {
-                if (inputStream.read() != (byte)0) {
+                byte testByte = (byte)inputStream.read();
+                if (testByte != (byte)0) {
                     throw new IllegalArgumentException("The format " +
                             "for PduParticipants is wrong! corrupt " +
                             "PDU");
                 }
+                sequenceBuilder.append(testByte); //för testerna
             }
         }
+        bytes = sequenceBuilder.toByteArray();
     }
 
     /**
@@ -59,5 +73,10 @@ public class PduParticipants extends Pdu{
         for (String participant : participants) {
             System.out.println(participant);
         }
+    }
+
+
+    public String[] getParticipants() {
+        return participants;
     }
 }
